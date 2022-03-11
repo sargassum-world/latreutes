a90a6e303e901d855efc4f5e9bcadc4b8b6e9cf1
Ethan Li <lietk12@gmail.com>
2022-03-10 18:04:17 -0800

    Apply formatting auto-fixes

--- src/zerotier/networks/joining/messages/AccessDenied.svelte
+++ src/zerotier/networks/joining/messages/AccessDenied.svelte
@@ -1,7 +1,7 @@
 <div class="message-header">Access denied</div>
 <div class="message-body">
   For security reasons, when you try to join this network you&apos;ll have to be
-  authorized by the network&apos;s host before you can actually access it. You may
-  need to contact the network administrator to request authorization; in the
+  authorized by the network&apos;s host before you can actually access it. You
+  may need to contact the network administrator to request authorization; in the
   meantime, you can close this panel or try to join another network.
 </div>
